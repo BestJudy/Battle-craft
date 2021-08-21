0dad30fb5b9a690c482b9369a51d033310fa8059
BestJudy <82187855+BestJudy@users.noreply.github.com>
2021-08-20 21:49:17 -0400

    Add files via upload

--- 43My game i made.py
+++ 43My game i made.py
@@ -8,22 +8,27 @@ pygame.display.set_caption("Battle Craft")
 WHITE = (98, 84, 234)
 
 FPS = 60
+VEL = 5
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 YELLOW_SPACE_IMAGE = pygame.image.load('./python02/spaceship_yellow.png')
-YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACE_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACE_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
 
 RED_SPACE_IMAGE = pygame.image.load('./python02/spaceship_red.png')
-RED_SPACESHIP = pygame.transform.scale(RED_SPACE_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACE_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
-def draw_window():
+def draw_window(red, yellow):
     WIN.fill((WHITE))
-    WIN.blit(YELLOW_SPACESHIP, (300, 100))
-    WIN.blit(RED_SPACESHIP, ())
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, yellow.y))
     pygame.display.update()
 
 
 def main():
+    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -32,7 +37,13 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        draw_window()
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_a]: #Left
+            yellow.x -= VEL
+        if keys_pressed[pygame.K_d]: #Right
+            yellow.x += VEL
+
+        draw_window(red, yellow)
 
     pygame.quit()
 
