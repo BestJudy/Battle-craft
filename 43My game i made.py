fd6050d07588227ffdceee227e4cf0abe336bffd
BestJudy <82187855+BestJudy@users.noreply.github.com>
2021-09-05 21:22:03 -0400

    Add files via upload

--- 43My game i made.py
+++ 43My game i made.py
@@ -35,17 +35,17 @@ def draw_window(red, yellow):
     pygame.display.update()
 
 def handle_bullets(yellow_bullets, red_bullets, yellow, red):
-    for bullet in yellow_bullets:
-        bullet.x += BULLET_VEL
-        if red.colliderect(bullet):
-            pygame.event.post(pygame.event.Event(RED_HIT))
-            yellow_bullets.remove(bullet)
+        for bullet in yellow_bullets:
+            bullet.x += BULLET_VEL
+            if red.colliderect(bullet):
+                pygame.event.post(pygame.event.Event(RED_HIT))
+                yellow_bullets.remove(bullet)
 
-    for bullet in red_bullets:
-        bullet.x -= BULLET_VEL
-        if yellow.colliderect(bullet):
-            pygame.event.post(pygame.event.Event(YELLOW_HIT))
-            red_bullets.remove(bullet)
+        for bullet in red_bullets:
+            bullet.x -= BULLET_VEL
+            if yellow.colliderect(bullet):
+                pygame.event.post(pygame.event.Event(YELLOW_HIT))
+                red_bullets.remove(bullet)
 
 def main():
     red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
